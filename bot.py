7c80a8f300803ea0fb97d9ce48f98195d254cb49
OMINDA DILSHAN RESPACT <85678266+omindadelshan@users.noreply.github.com>
2021-10-15 21:02:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,5 +9,9 @@ app = Client("tagremover1", bot_token = TOKEN, api_id = API_ID, api_hash = API_H
 @app.on_message(filters.private & filters.text | filters.media)
 async def tag(client, message):
   await message.copy(message.chat.id)
+  
+@app.on_message(filters.command(['start']))
+def start(client, message):
+  await message.reply_text(text =f"Hello **{message.from_user.first_name }** \n\n **Iam Simple web scraper** ðŸ•¸ \n __SEND ME WEBSITE LINK AND GET THAT WEB SOURCE__",)
 
 app.run()
