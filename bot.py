02bfbffed60a43701ac81447d712d652459e2d02
OMINDA DILSHAN RESPACT <85678266+omindadelshan@users.noreply.github.com>
2021-10-15 20:46:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,13 @@
 from pyrogram import Client,filters
 
-API_ID = ""
-API_HASH = ""
-TOKEN =""
+API_ID = "6021226"
+API_HASH = "7c6dd7679f9dc0ab599f336de13cedf1"
+TOKEN ="2092590107:AAFld-bHnflQ7Ra1RN-daiPaUuYeHC_Q_kI"
 
 app = Client("tagremover1", bot_token = TOKEN, api_id = API_ID, api_hash = API_HASH)
 
 @app.on_message(filters.private & filters.text | filters.media)
 async def tag(client, message):
-  await message 
-  message.copy(message.chat.id)
+  await message.copy(message.chat.id)
 
 app.run()
