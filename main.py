cc5ca1984be7186134be0f4c5a6226cacac075c1
OMINDA DILSHAN RESPACT <85678266+omindadelshan@users.noreply.github.com>
2021-10-15 21:51:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from pyrogram import Client,filters 
 
-API_ID = "" 
-API_HASH = "" 
-TOKEN ="" 
+API_ID = "6021226" 
+API_HASH = "7c6dd7679f9dc0ab599f336de13cedf1" 
+TOKEN ="2092590107:AAFld-bHnflQ7Ra1RN-daiPaUuYeHC_Q_kI" 
 
 app = Client("tagremover1", bot_token = TOKEN, api_id = API_ID, api_hash = API_HASH) 
 
